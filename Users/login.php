fbbe21dc86d6b6f8a93b8288ab83dbe61654799f
JoseAntonioMartinez2525 <63381930+JoseAntonioMartinez2525@users.noreply.github.com>
2020-11-26 22:18:28 -0700

    Update login.php

--- Users/login.php
+++ Users/login.php
@@ -5,14 +5,27 @@ if (!isset($_SESSION)) {
 
 include "../app/connectionCtrl.php";
 
+<?php
+
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>User</title>
+		<style type="text/css">
+		fieldset{
+			width: 50%;
+			margin: auto;
+			background-color: lightblue;
+			
+			padding: 20px;
+		}
+	</style>
 </head>
 <body>
-	<fieldset>Register</fieldset>
+
+	<fieldset>Register
 	<form method="POST" action="app/authController.php.">
 		<legend>Register</legend>
 		<label>name</label>
@@ -25,17 +38,18 @@ include "../app/connectionCtrl.php";
 		<input type="hidden" name="action" value="register">
 		
 	</form>
-<form>
+	</fieldset>
+<form><br>
 	<fieldset>
 	Login
-</fieldset>
+
 <label>email</label>
 		<input type="email" name="email" required="">
 			<label>password</label>
 		<input type="password" name="password" required=""><br>
 		<button type="submit">Acess</button>	
 		<input type="hidden" name="action" value="login">
-
+</fieldset>
 </form>
 </body>
 </html>
